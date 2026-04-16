d627401b3dac2c9ffb0b9fe6ffe35e727f995ac0
fernandovmedina <fernandovazquez.favm@gmail.com>
2026-04-16 11:51:28 -0600

    16/04/26 --2

--- remove_element.py
+++ remove_element.py
@@ -1,5 +1,26 @@
 class Solution:
   def removeElement(self, nums: list[int], val: int) -> int:
-    temp: int = float("inf")
+    k = 0
+
     for i in range(len(nums)):
-      
+      if nums[i] != val:
+        nums[k] = nums[i]
+        k += 1
+
+    for i in range(k, len(nums)):
+      nums[i] = 0
+    return k
+
+nums = [3,2,2,3]
+val = 3
+
+nums2 = [0,1,2,2,3,0,4,2]
+val2 = 2
+
+x = Solution()
+
+j = x.removeElement(nums2, val2)
+k = x.removeElement(nums, val)
+
+print(k, nums)
+print(j, nums2)
