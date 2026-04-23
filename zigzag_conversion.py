2127bde59f83d14d16b626c0196906592c005825
fernandovmedina <fernandovazquez.favm@gmail.com>
2026-04-22 23:46:42 -0600

    22/04/26 --2

--- zigzag_conversion.py
+++ zigzag_conversion.py
@@ -1,7 +1,23 @@
 class Solution:
   def convert(self, s: str, numRows: int) -> str:
-    
-    print()
+    if numRows == 1 or numRows >= len(s):
+      return s
+
+    rows = [""] * numRows
+    i = 0
+    step = 1
+
+    for c in s:
+      rows[i] += c
+
+      if i == 0:
+        step = 1
+      elif i == numRows - 1:
+        step = -1
+
+      i += step
+
+    return "".join(rows)
 
 x = Solution()
 
