6a4b1d6af0435c6ba25664e8165155f247411f04
fernandovmedina <fernandovazquez.favm@gmail.com>
2026-05-12 16:45:10 -0600

    12/05/26 --1

--- leetcode/11.py
+++ leetcode/11.py
@@ -1,18 +1,23 @@
 class Solution:
-  def maxArea(self, height: list[int]) -> int:
-    tmp1: int = 0
-    tmp2: int = -999
-    for i in range(len(height)):
-      if height[i] > tmp1:
-        tmp1 = height[i]
-    for i in range(len(height)):
-      if height[i] > tmp2 and height[i] < tmp1:
-        tmp2 = height[i]
-    if tmp2 == -999:
-      tmp2 = tmp1
-    return tmp2 * tmp2
+    def maxArea(self, height: list[int]) -> int:
+      left = 0
+      right = len(height) - 1
+      max_area = 0
+
+      while left < right:
+        h = min(height[left], height[right])
+        w = right - left
+
+        max_area = max(max_area, h * w)
+
+        if height[left] < height[right]:
+          left += 1
+        else:
+          right -= 1
+
+      return max_area
 
 x = Solution()
 
-x.maxArea(height = [1,8,6,2,5,4,8,3,7]) # Output: 56
-x.maxArea(height = [1,1]) # Output: 1
+print(x.maxArea(height = [1,8,6,2,5,4,8,3,7])) # Output: 49
+print(x.maxArea(height = [1,1])) # Output: 1
