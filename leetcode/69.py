4c5f85bb23674660b34e99fc2878063d4b3d6ffd
fernandovmedina <fernandovazquez.favm@gmail.com>
2026-05-15 00:28:17 -0600

    15/05/26 --1

--- leetcode/69.py
+++ leetcode/69.py
@@ -1,6 +1,24 @@
 class Solution:
   def mySqrt(self, x: int) -> int:
-    return int(x ** 1/2)
+    if x < 2:
+      return x
+
+    left = 1
+    right = x // 2
+
+    while left <= right:
+      mid = (left + right) // 2
+      square = mid * mid
+
+      if square == x:
+        return mid
+
+      if square < x:
+        left = mid + 1
+      else:
+        right = mid - 1
+
+    return right
 
 import sys
 
